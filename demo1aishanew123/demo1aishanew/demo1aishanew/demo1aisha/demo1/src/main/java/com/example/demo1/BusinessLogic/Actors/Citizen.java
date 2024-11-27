d5ea3f1a7bb2e8c9088f8b5138715f95b0d0d926
HaniaAamer <i221154@nu.edu.pk>
2024-11-27 19:14:08 +0500

    id of citizen

--- demo1aishanew123/demo1aishanew/demo1aishanew/demo1aisha/demo1/src/main/java/com/example/demo1/BusinessLogic/Actors/Citizen.java
+++ demo1aishanew123/demo1aishanew/demo1aishanew/demo1aisha/demo1/src/main/java/com/example/demo1/BusinessLogic/Actors/Citizen.java
@@ -2,9 +2,12 @@ package com.example.demo1.BusinessLogic.Actors;
 
 // Subclass: Citizen
 public class Citizen extends User {
+   private String citizenID;
     // Constructor for Citizen
     public Citizen(String area, String name, String password, String phone, String cnic) {
         super(area, name, password, phone, cnic); // Call the constructor of the parent class
+
+        this.citizenID = cnic;
     }
 
 }
